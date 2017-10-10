f3fe28d453021b27a6073a2f100e6d7fa609bc6a
zhangxiahui <281055769@qq.com>
2017-10-10 10:46:45 +0800

    #TS-IMIS前端项目 模板管理样式微调

--- js/filter.js
+++ js/filter.js
@@ -336,3 +336,42 @@ app.filter('handOverType',function(){
         return resultType;
     }
 });
+
+app.directive('resize', function ($window) {
+    return function (scope, element) {
+        var w = angular.element($window);
+        scope.getWindowDimensions = function () {
+            return { 'h': w.height(), 'w': w.width() };
+        };
+        scope.$watch(scope.getWindowDimensions, function (newValue, oldValue) {
+            scope.windowHeight = newValue.h;
+            scope.windowWidth = newValue.w;
+
+            scope.style = function () {
+                return {
+                    'height': (newValue.h - 100) + 'px',
+                    'width': (newValue.w - 100) + 'px'
+                };
+            };
+
+        }, true);
+
+        w.bind('resize', function () {
+            scope.$apply();
+        });
+    }
+});
+
+app.directive('autoHeight',function ($window) {
+    return {
+        restrict : 'A',
+        scope : {},
+        link : function($scope, element, attrs) {
+            var winowHeight = $window.innerHeight; //获取窗口高度
+            var headerHeight = 80;
+            var footerHeight = 20;
+            element.css('min-height',
+                (winowHeight - headerHeight - footerHeight) + 'px');
+        }
+    };
+});

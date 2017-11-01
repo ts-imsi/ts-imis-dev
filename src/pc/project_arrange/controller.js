754282ae330744571591c58304f6dd61dba5af5d
luoyunsak <sakfi102012@163.com>
2017-11-01 10:47:55 +0800

    #项目安排修改

--- src/pc/project_arrange/controller.js
+++ src/pc/project_arrange/controller.js
@@ -1,6 +1,6 @@
 app.controller('projectArrangeCtrl', ['$scope', '$modal', '$http', '$filter','$log', function ($scope, $modal, $http,$filter, $log) {
     var selt = this;
-
+    selt.showLetter=false;
 
     selt.isArrange=0;
     this.selectByStatus=function(status){
@@ -57,4 +57,60 @@ app.controller('projectArrangeCtrl', ['$scope', '$modal', '$http', '$filter','$l
     this.maxSize = 5;
     this.setPage(1);
 
+    $http.post("/ts-project/arrange/getManageByType/"+"1").success(function (result) {
+        if(result.success){
+            selt.xmjlList=result.object;
+        }else{
+            selt.xmjlList=[];
+            alert(result.message);
+        }
+
+    });
+
+    this.changeLetter=function(handOver){
+        selt.pkid=handOver.pkid;
+    }
+
+    this.ctreadLetter=function(handOver){
+        var letterInstance = $modal.open({
+            templateUrl: 'Letter.html',
+            controller: 'LetterCtrl as letterCtrl',
+            size: 'lg',
+            resolve: {
+                data: function () {
+                    return handOver;
+                }
+            }
+        });
+
+        letterInstance.result.then(function () {
+        });
+    }
 }]);
+
+
+app.controller('LetterCtrl', ['$scope', '$modalInstance','$http', 'data', function($scope,$modalInstance,$http,data) {
+    var seltLetter=this;
+    seltLetter.handOver=data;
+    seltLetter.newDate=new Date();
+
+    this.close=function () {
+        $modalInstance.close();
+    };
+
+    this.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+    this.printLetter=function(){
+        $("#letterPrint").printThis({
+            debug: false,
+            importCSS: false,
+            importStyle: false,
+            printContainer: true,
+            removeInline: false,
+            printDelay: 333,
+            header: null,
+            formValues: false
+        });
+    }
+}])

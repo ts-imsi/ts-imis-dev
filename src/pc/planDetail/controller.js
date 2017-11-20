5b1cd05ae7ca5c9d4844bea8060868ba59d4c0c6
zhangxiahui <281055769@qq.com>
2017-11-20 09:58:06 +0800

    #TS-IMIS前端项目 项目管理制定计划

--- src/pc/planDetail/controller.js
+++ src/pc/planDetail/controller.js
@@ -1,5 +1,7 @@
-app.controller('planDetailCtrl', ['$scope', '$modal', '$http', '$filter','$log', function ($scope, $modal, $http,$filter, $log) {
+app.controller('planDetailCtrl', ['$scope', '$modal', '$http', '$filter','$log','utils', function ($scope, $modal, $http,$filter, $log,utils) {
     var selt = this;
+
+    var planId =  utils.getUrlVar('planId');
 
     //制定计划划出层样式
     this.panelClass = "contact panel panel-default";
@@ -9,5 +11,48 @@ app.controller('planDetailCtrl', ['$scope', '$modal', '$http', '$filter','$log',
     };
     this.closePanel = function () {
         selt.panelClass = "contact panel panel-default";
-    }
+    };
+
+
+
+
+    $http.post("/ts-project/planDetail/queryPlanItems/"+planId).success(function (result) {
+        if(result.success){
+            selt.detail=result.object;
+        }
+    });
+
+
+    this.dateOptions = {
+        formatYear: 'yy',
+        startingDay: 1,
+        class: 'datepicker'
+    };
+    this.formats = ['yyyy-MM-dd', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+    this.format = this.formats[0];
+    this.openTime = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        selt.isOpen = true;
+    };
+
+    this.showDate=function(pkid,sz){
+        selt.isShow=pkid;
+        if(sz==0){
+            selt.name="planStartTime";
+        }
+        if(sz==1){
+            selt.name="planEndTime";
+        }
+        if(sz==2){
+            selt.name="startTime";
+        }
+        if(sz==3){
+            selt.name="endTime";
+        }
+
+    };
+
+
 }]);

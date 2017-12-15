58b3c52e839bb0796f07ce5a8ea91ea4c78c8380
luoyunsak <sakfi102012@163.com>
2017-12-15 14:36:51 +0800

    #修改遮罩层

--- js/directives/loading.js
+++ js/directives/loading.js
@@ -2,12 +2,21 @@ angular.module('app').directive('loading', function() {
     return {
         replace: true,
         restrict: 'AE',
-        template:'<div class="back-layer"><div class="loading">'
+        template:'<div class="back-layer"><div class="mask"></div><div class="loading">'
         +'<img src="/img/loading.gif">'
         +'</div></div>',
         link: function($scope, $element, attrs) {
             var top = $(window).height()/2 - 25;
             var left = $(window).width()/2 - 25;
+/*            $('.mask').css({
+                top: 0,
+                left: 100,
+                position: 'fixed',
+                width: '100%',
+                height: '100%',
+                opacity: '0.5',
+                background: 'black'
+            });*/
             $('.loading').css({
                 left: left,
                 top:top,

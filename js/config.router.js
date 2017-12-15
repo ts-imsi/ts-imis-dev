e82c32b1c4325854704d70a452d7e35a6870a2a5
luoyunsak <sakfi102012@163.com>
2017-12-15 11:11:55 +0800

    #加载框提示

--- js/config.router.js
+++ js/config.router.js
@@ -15,7 +15,7 @@ angular.module('app')
   .config(
     [          '$stateProvider', '$urlRouterProvider','$httpProvider',
       function ($stateProvider,   $urlRouterProvider,$httpProvider) {
-
+          $httpProvider.interceptors.push('loadingInterceptor');
           $httpProvider.defaults.headers.common = { 'X-TOKEN' : sessionStorage.getItem("X-TOKEN") }
           
           $urlRouterProvider
@@ -947,5 +947,40 @@ angular.module('app')
               })
       }
     ]
-  );
+  ) .directive('loading', function() {
+    return {
+        replace: true,
+        restrict: 'AE',
+        template:'<div class="back-layer"><div class="loading">'
+        +'<img src="/img/loading.gif">'
+        +'</div></div>',
+        link: function($scope, $element, attrs) {
+            var top = $(window).height()/2 - 25;
+            var left = $(window).width()/2 - 25;
+            $('.loading').css({
+                top: top,
+                left: left
+            });
+            //$(tpl).appendTo('body');
+        }
+    };
+})
+
+    .factory('loadingInterceptor', function($q, $rootScope) {
+
+        return {
+            request: function(config) {
+                $(".back-layer").show();
+                return config || $q.when(config);
+            },
+            response: function(response) {
+                $(".back-layer").hide();
+                return response || $q.when(response);
+            },
+            responseError: function(rejection) {
+                $(".back-layer").hide();
+                return $q.reject(rejection);
+            }
+        };
+    });
 

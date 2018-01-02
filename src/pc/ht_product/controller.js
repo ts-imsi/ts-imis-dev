04e9ab81346496b05fb495b54b806ff6e84678ff
zhangxiahui <281055769@qq.com>
2018-01-02 10:24:46 +0800

    #TS-IMIS 交接单保存修改

--- src/pc/ht_product/controller.js
+++ src/pc/ht_product/controller.js
@@ -494,7 +494,7 @@ app.controller('htProductCtrl', ['$scope', '$modal', '$http', '$filter','$log', 
         if(keepGoing){
             if(selt.handover.status&&selt.handover.status!=0
                 &&selt.handover.nowStep
-                &&(selt.handover.nowStep!='待内控审批'||selt.handover.nowStep!='待销售提交交接单')){
+                &&(selt.handover.nowStep!='待内控审批'&&selt.handover.nowStep!='待销售提交交接单')){
                 alert("该交接单已经提交,不能修改!");
                 return;
             }

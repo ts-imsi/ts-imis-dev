45f9b9a838323e4113be80eccc352ae8c8bbd99c
luoyunsak <sakfi102012@163.com>
2018-01-12 16:47:42 +0800

    #修改bug

--- src/mobile/makeInTransfer/controller.js
+++ src/mobile/makeInTransfer/controller.js
@@ -3,6 +3,31 @@
         .module('MOBILEAPP.MAKEINTRANSFER.CONTROLLER', ['ui.bootstrap'])
         .controller('makeInCtrl', ['$http','$uibModal','$log','$document','utils',function($http,$uibModal, $log, $document,utils) {
             var selt = this;
+
+            selt.outputValueM=false;
+            selt.confirValueM=false;
+            selt.transactM=false;
+            selt.handoverM=false;
+            selt.projectMentM=false;
+            $http.get("/ts-authorize/ts-mobile/menus").success(function(result){
+                if(result.success){
+                    selt.munuList=result.object[0].childrens;
+                    angular.forEach(selt.munuList,function(item){
+                        if(item.translate=='outputValue'){
+                            selt.outputValueM=true;
+                        }else if(item.translate=='confirValue'){
+                            selt.confirValueM=true;
+                        }else if(item.translate=='transact'){
+                            selt.transactM=true;
+                        }else if(item.translate=='handover'){
+                            selt.handoverM=true;
+                        }else if(item.translate=='projectMent'){
+                            selt.projectMentM=true;
+                        }
+                    })
+                }
+            });
+
             this.queryOaContractListByOwner=function(){
                 var param={
                     showAll:selt.showAll

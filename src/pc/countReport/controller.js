9b2e020d77a057c56f8214f1ed3eaa133a4de510
luoyunsak <sakfi102012@163.com>
2018-01-15 09:42:57 +0800

    #内控产值前端

--- src/pc/countReport/controller.js
+++ src/pc/countReport/controller.js
@@ -2,6 +2,51 @@ app.controller('CountReport', ['$scope', '$modal', '$http', '$filter','$log', fu
     var selt = this;
 
     this.year = $filter("date")(new Date(), "yyyy");
+    selt.showAll=true;
+    selt.showPro=false;
+    selt.showProLine=false;
+    this.selectByType=function(showType) {
+        if (showType == 'all') {
+            selt.showAll=true;
+            selt.showPro=false;
+            selt.setPage(1);
+        } else {
+            selt.showPro = true;
+            selt.showAll = false;
+            if(showType=='pro'){
+                selt.showProLine=true;
+            }else{
+                selt.showProLine=false;
+            }
+            selt.totalCount = 0;
+            selt.finishedCount = 0;
+            selt.unfinishedCount = 0;
+            selt.nextUnCount=0;
+            selt.countReportByType(showType);
+        }
+    }
+
+    this.countReportByType=function(showType){
+        var param={
+            year:selt.year,
+            selectType:showType
+        }
+
+        $http.post("/ts-project/countReport/getCountReport",angular.toJson(param)).success(function (result) {
+            if(result.success){
+                selt.countRPro = result.object;
+                angular.forEach(selt.countRPro,function(item){
+                    selt.unfinishedCount=selt.unfinishedCount+item.unfinished;
+                    selt.finishedCount=selt.finishedCount+item.finished;
+                    selt.totalCount=selt.totalCount+item.total;
+                    selt.nextUnCount=selt.nextUnCount+item.lastUnFinished;
+                });
+            }else{
+                selt.countRPro=[];
+            }
+
+        });
+    }
 
     this.setPage = function (pageNo) {
         var parm={

f022c41f220f1f0ee51db70a02c8633b331c9a00
luoyunsak <sakfi102012@163.com>
2018-02-07 10:56:44 +0800

    #修改栏目bug

--- src/mobile/project-mament/controller.js
+++ src/mobile/project-mament/controller.js
@@ -12,20 +12,28 @@
             selt.projectMentM=false;
             $http.get("/ts-authorize/ts-mobile/menus").success(function(result){
                 if(result.success){
-                        selt.munuList=result.object[0].childrens;
-                        angular.forEach(selt.munuList,function(item){
-                            if(item.translate=='outputValue'){
-                                selt.outputValueM=true;
-                            }else if(item.translate=='confirValue'){
-                                selt.confirValueM=true;
-                            }else if(item.translate=='transact'){
-                                selt.transactM=true;
-                            }else if(item.translate=='handover'){
-                                selt.handoverM=true;
-                            }else if(item.translate=='projectMent'){
-                                selt.projectMentM=true;
+                    if(result.object[0].length!=0) {
+                        selt.munuList = result.object[0].childrens;
+                        angular.forEach(selt.munuList, function (item) {
+                            if (item.translate == 'outputValue') {
+                                selt.outputValueM = true;
+                            } else if (item.translate == 'confirValue') {
+                                selt.confirValueM = true;
+                            } else if (item.translate == 'transact') {
+                                selt.transactM = true;
+                            } else if (item.translate == 'handover') {
+                                selt.handoverM = true;
+                            } else if (item.translate == 'projectMent') {
+                                selt.projectMentM = true;
                             }
-                        })
+
+                        });
+                    }else{
+                        selt.munuList = [];
+                    }
+
+                }else{
+                    selt.munuList = [];
                 }
             });
 
--- src/mobile/userController.js
+++ src/mobile/userController.js
@@ -33,20 +33,25 @@
             selt.munuList=[];
             $http.get("/ts-authorize/ts-mobile/menus").success(function(result){
                 if(result.success){
-                    selt.munuList=result.object[0].childrens;
-                    angular.forEach(selt.munuList,function(item){
-                        if(item.translate=='outputValue'){
-                            selt.outputValueM=true;
-                        }else if(item.translate=='confirValue'){
-                            selt.confirValueM=true;
-                        }else if(item.translate=='transact'){
-                            selt.transactM=true;
-                        }else if(item.translate=='handover'){
-                            selt.handoverM=true;
-                        }else if(item.translate=='projectMent'){
-                            selt.projectMentM=true;
-                        }
-                    });
+                    if(result.object[0].length!=0) {
+                        selt.munuList = result.object[0].childrens;
+                        angular.forEach(selt.munuList, function (item) {
+                            if (item.translate == 'outputValue') {
+                                selt.outputValueM = true;
+                            } else if (item.translate == 'confirValue') {
+                                selt.confirValueM = true;
+                            } else if (item.translate == 'transact') {
+                                selt.transactM = true;
+                            } else if (item.translate == 'handover') {
+                                selt.handoverM = true;
+                            } else if (item.translate == 'projectMent') {
+                                selt.projectMentM = true;
+                            }
+
+                        });
+                    }else{
+                        selt.munuList = [];
+                    }
 
                 }else{
                     selt.munuList = [];
